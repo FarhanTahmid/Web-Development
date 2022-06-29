00ddb607ed2fcb953ff1d796e024e0ec391d4b3a
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-29 21:08:43 +0600

    sending data from template

--- DJANGO/myProject/myApp/views.py
+++ DJANGO/myProject/myApp/views.py
@@ -1,6 +1,12 @@
+from multiprocessing import context
 import re
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    context={
+        'name':'farhan',
+        'age':22,
+        'nationality':'Bangladeshi',
+    }
+    return render(request,'index.html',context)
