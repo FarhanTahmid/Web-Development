be55f4022690ddd0127390147f5c56fc13a67aa0
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-29 20:30:23 +0600

    integrating templates

--- DJANGO/myProject/myApp/views.py
+++ DJANGO/myProject/myApp/views.py
@@ -1,5 +1,6 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>hey welcome here!</h1>')
+    return render(request,'index.html')
