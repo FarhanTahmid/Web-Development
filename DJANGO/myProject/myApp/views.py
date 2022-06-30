658469fd8cd059bc037c64637a66d5044094c352
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-30 13:16:02 +0600

    problem rendering html

--- DJANGO/myProject/myApp/views.py
+++ DJANGO/myProject/myApp/views.py
@@ -1,12 +1,16 @@
 from multiprocessing import context
-import re
+from .models import Feature
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    feature1=Feature()
+    feature1.name='Farhan'
+    feature1.id=10
+    feature1.description="A very good boy"
     context={
         'name':'farhan',
         'age':22,
         'nationality':'Bangladeshi',
     }
-    return render(request,'index.html',context)
+    return render(request,'index.html',context,{'feature':feature1})
