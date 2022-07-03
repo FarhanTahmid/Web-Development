0016c93f0f54a279a495397df936273b70ea18f0
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-04 01:53:38 +0600

    admin site

--- DJANGO/django_models/django_models_app/views.py
+++ DJANGO/django_models/django_models_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    feature1=Feature()
+    feature1.id=10
+    feature1.name="Farhan"
+    feature1.detail="Web developer"
+
+    return render(request,'index.html',{'feature':feature1})
