ead2528934cdd9b0d560c2204eafbfdd85805b30
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-07 08:29:18 +0600

    admin panel and database in admin panel

--- DJANGO/django_models/django_models_app/views.py
+++ DJANGO/django_models/django_models_app/views.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 from .models import Feature
 # Create your views here.
 def index(request):
-    feature1=Feature()
-    feature1.id=10
-    feature1.name="Farhan"
-    feature1.detail="Web developer"
+    features=Feature.objects.all()
 
-    return render(request,'index.html',{'feature':feature1})
+    return render(request,'index.html',{'feature':features})
