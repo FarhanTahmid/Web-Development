8e115bc37f73db70c2e587afff276c79f2495e24
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-07 09:04:31 +0600

    file structure of user registration

--- DJANGO/UserRegistration/UserRegistrationApp/views.py
+++ DJANGO/UserRegistration/UserRegistrationApp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def userRegistration(request):
+    return render(request,'userRegistration.html')
